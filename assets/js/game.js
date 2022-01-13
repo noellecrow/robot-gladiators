88de9bb45d35b295c7a84326c96b9b486a7f7ba6
noellecrow <crownoelle@gmail.com>
2022-01-13 10:19:28 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,24 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player's robot's health is zero or less
+
 // var playerName =
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
+    // fight function statements
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -68,4 +74,6 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+};
