758780d64752f4b3cb2e55cd90992afc7f137d3f
noellecrow <crownoelle@gmail.com>
2022-01-21 11:03:55 -0800

    player-name fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -158,8 +158,19 @@ for (var i = 0; i< enemyInfo.length; i++) {
      }
  };
 
+ // function to set name
+ var getPlayerName = function() {
+     var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+     console.log("Your robot's name is " + name);
+     return name;
+ };
  var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
